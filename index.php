4264a2d2c3c4480383a8b8b5ee6963a5ff0af81f
zustinanasco <zustinanasco0@gmail.com>
2026-08-21 12:30:20 +0800

    ADDED SETSUNA HANABI

--- index.php
+++ index.php
@@ -14,3 +14,20 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SETSUNA HANABI</title>
+</head>
+<body>
+    <form method="POST">
+        <button
+            type="submit"
+            name="action"
+        >Sample</button>
+    
+</body>
+</html>
