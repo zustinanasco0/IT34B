1f2e626b87e8dc200874ce49e8da811f9f56625a
zustinanasco <zustinanasco0@gmail.com>
2026-09-14 15:05:12 +0800

    added activity logs for log in and log out session

--- auth/signout.php
+++ auth/signout.php
@@ -2,6 +2,11 @@
 
 require_once '../config/config.php';
 
+if(isset($_SESSION['user_id'])){
+    logActivity($pdo,$_SESSION['user_id']),$_SESSION['user_id']) ;
+    
+}
+
 $_SESSION = [];
 
 session_destroy();
--- index.php
+++ index.php
@@ -12,12 +12,37 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $login = trim($_POST['login'] ?? '' );
     $password = $_POST['password'] ?? '';
     
+
     if(loginUser($pdo,$login,$password)){
         header('Location:' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
 
     }
 
     $error = 'Invalid Login Credentials';
+
+    if ($login==='' || $password === '') {
+        // log incomplete
+        logActivity($pdo,null,$login,'login','failed') ;
+
+    } else {
+
+    if(loginUser($pdo,$login,$password)){
+
+ logActivity( 
+    $pdo,$_SESSION['user_id'],
+    $_SESSION['user_email'],
+
+    'login',
+    'success'
+
+
+
+
+ )
+
+    }
+
+
 
 }
 
